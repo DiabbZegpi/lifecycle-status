dc56a31cd27b8630d8bc16d51c02eedbcd40a0b4
Diabb Zegpi <diabbluis@gmail.com>
2024-06-11 17:20:30 -0400

    Import data. TO DO: Fix SKU to integer type

---
+++ R/02_feature_engineering.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(here)
+
+# Import data -------------------------------------------------------------
+master_data <- read_rds(here("pre-processed data", "master_data.rds"))
+active_sku <- read_rds(here("pre-processed data", "active_sku.rds"))
+sell_in <- read_rds(here("pre-processed data", "sell_in.rds"))
+forecast <- read_rds(here("pre-processed data", "forecast.rds"))
