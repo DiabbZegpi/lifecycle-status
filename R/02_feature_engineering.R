6580028f0f9e523f836744484554ff408d1916a2
Diabb Zegpi <diabbluis@gmail.com>
2024-06-12 16:45:28 -0400

    Aggregate at base code level. Had to omit NAs after the merge

--- R/02_feature_engineering.R
+++ R/02_feature_engineering.R
@@ -9,4 +9,17 @@ sell_in <- read_rds(here("pre-processed data", "sell_in.rds"))
 forecast <- read_rds(here("pre-processed data", "forecast.rds"))
 
 # Aggregate at base code level --------------------------------------------
-sell_in
+bcode_sales <-
+  sell_in |>
+  left_join(
+    master_data,
+    by = join_by(COUNTRY, SKU),
+    na_matches = "never",
+    relationship = "many-to-one"
+  ) |>
+  group_by(PERIOD, COUNTRY, BASECODE, APO_CUST_LEVEL2_CODE) |>
+  summarize(
+    SELL_IN_VOL = sum(SELL_IN_VOL),
+    .groups = "drop"
+  ) |>
+  na.omit()
