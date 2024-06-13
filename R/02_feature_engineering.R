4f2da4649e7736dc05cf6ab0ee901d06d19f5179
Diabb Zegpi <diabbluis@gmail.com>
2024-06-13 19:21:53 -0400

    Feature engineering for history and parallel backend to speed-up (by 10-20%) the computations

--- R/02_feature_engineering.R
+++ R/02_feature_engineering.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(bit64)
 library(here)
+library(furrr)
 
 # Import data -------------------------------------------------------------
 master_data <- read_rds(here("pre-processed data", "master_data.rds"))
@@ -23,3 +24,58 @@ bcode_sales <-
     .groups = "drop"
   ) |>
   na.omit()
+
+# bcode_lifecycle <-
+#   active_sku |>
+#   distinct(COUNTRY, BASECODE, APO_CUST_LEVEL2_CODE, LIFECYCLE_BASECODE) |>
+#   right_join(
+#     bcode_sales,
+#     by = join_by(COUNTRY, BASECODE, APO_CUST_LEVEL2_CODE),
+#     na_matches = "never",
+#     relationship = "one-to-many"
+#   ) |>
+#   na.omit()
+
+
+# Feature engineering on history ------------------------------------------
+last_period_observed <- max(bcode_lifecycle$PERIOD)
+
+history_features <-
+  bcode_sales |>
+  group_by(COUNTRY, BASECODE, APO_CUST_LEVEL2_CODE) |>
+  arrange(PERIOD, .by_group = TRUE) |>
+  # Lag-1 calculations
+  # May be necessary to add lag-n for seasonal vs in & out discrimination
+  mutate(
+    lag_1 = lag(SELL_IN_VOL, 1L),
+    lag_1 = if_else(is.na(lag_1), SELL_IN_VOL, lag_1),
+    drop_zero_flag = case_when(
+      lag_1 > 0 & SELL_IN_VOL == 0 ~ "drop_to_zero",
+      lag_1 == 0 & SELL_IN_VOL > 0 ~ "raise_from_zero",
+      lag_1 == 0 & SELL_IN_VOL == 0 ~ "constant_zero",
+      .default = "constant_positive"
+    )
+  ) |>
+  select(-matches("^lag_\\d{1,2}$")) |>
+  ungroup()
+
+# Parallel backend
+workers <- parallel::detectCores(logical = FALSE)
+plan(multisession, workers = workers)
+
+history_features |>
+  nest(history = c(PERIOD, SELL_IN_VOL, drop_zero_flag)) |>
+  mutate(
+    history_36 = future_map(history, ~ slice_tail(.x, n = 36)),
+    history_24 = future_map(history, ~ slice_tail(.x, n = 24)),
+    history_12 = future_map(history, ~ slice_tail(.x, n = 12))
+  )
+  summarize(
+    positive_last_12 = sum(SELL_IN_VOL > 0),
+    zeros_last_12 = sum(SELL_IN_VOL == 0),
+    .groups = "drop"
+  )
+
+
+
+
